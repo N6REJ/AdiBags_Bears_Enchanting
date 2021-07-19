08b5fe38615976eadb131bdaa460ce24ad22958b
Bear <programming@hallhome.us>
2021-07-19 18:28:38 -0500

    Add files via upload

---
+++ database.lua
@@ -0,0 +1,22 @@
+-- AdiBags_Shadowlands_Crafting - Enchanting Database
+-- Created by N6REJ character is Bearesquishy - dalaran please credit whenever.
+-- Source on GitHub: https://github.com/N6REJ/AdiBags_Shadowlands_Crafting
+--
+local ADDON_NAME, ADDON_TABLE, addon = ...
+
+-- Create addon table
+N = { }
+ADDON_TABLE.N = N
+
+-- Label to use
+N["FilterTitle"] = "Enchanting"
+
+-- Database of items
+N["database"] = {
+	172230,	-- Soul Dust
+	172231,	-- Sacred Shard
+	172232,	-- Eternal Crystal
+	172439,	-- Enchanted Lightless Silk
+	183951,	-- Immortal Shard
+	38682,	-- Enchanting Vellum
+}
