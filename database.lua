8e6594ad997e7b98c7a47969c7bd43da2c0a9da0
N6REJ <programming@hallhome.us>
2021-11-10 18:03:12 -0600

    Restructured database to match other addons. Added missing items. Renamed "Tailoring" to "Cloth"

--- database.lua
+++ database.lua
@@ -12,20 +12,51 @@ db.desc = "Enchanting reagents for shadowlands"
 
 -- Filter info
 db.Filters = {
-	["Enchanting"] = {
-		uiName = "Shadowlands Enchanting",
-		uiDesc = "Enchanting reagents for Shadowlands",
-		title = "Enchanting",
-		items = {
-			-- ID,	--Item name
-			[172230] = true, 	-- Soul Dust
-			[172231] = true, 	-- Sacred Shard
-			[172232] = true, 	-- Eternal Crystal
-			[172439] = true, 	-- Enchanted Lightless Silk
-			[183951] = true, 	-- Immortal Shard
-			[38682] = true, 	-- Enchanting Vellum
-		},
-	},
+    ["Enchanting"] = {
+        uiName = "Shadowlands Enchanting",
+        uiDesc = "Enchanting reagents for Shadowlands",
+        title = "Enchanting",
+        items = {
+            -- ID,	--Item name
+            [172230] = true, -- Soul Dust
+            [172231] = true, -- Sacred Shard
+            [172232] = true, -- Eternal Crystal
+        },
+    },
+    ["Cloth"] = {
+        uiName = "Shadowlands Tailoring",
+        uiDesc = "Reagents for tailoring in Shadowlands",
+        title = "Cloth",
+        items = {
+            -- ID    = true,    --Item name
+            [172439] = true, -- Enchanted Lightless Silk
+        }
+    },
+    ["Vendor_Reagents"] = {
+        uiName = "Shadowlands Vendor Reagents",
+        uiDesc = "Reagents from vendors used in Shadowlands",
+        title = "Vendor Reagents",
+        items = {
+            -- ID    = true,    --Item name
+            [183951] = true, -- Immortal shards
+            [38682] = true, -- Enchanting Vellum
+
+        }
+    },
+    ["Crafted_Reagents"] = {
+        uiName = "Shadowlands crafted reagents",
+        uiDesc = "Reagents made by crafting in Shadowlands",
+        title = "Crafted Reagents",
+        items = {
+            -- ID    = true,    --Item name
+            [173384] = true, -- Crafter's Mark of the Chained Isle
+            [173383] = true, -- Crafter's Mark III
+            [173382] = true, -- Crafter's Mark II
+            [173381] = true, -- Crafter's Mark I
+            [183942] = true, -- Novice Mark II
+            [185960] = true, -- Vestige of Origins
+        }
+    },
 }
 
 -- now that db is populated lets pass it on.
